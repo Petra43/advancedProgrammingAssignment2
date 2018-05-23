c195ca969757759560bb83e74527451d977c3dd7
Smarine43 <32082976+Smarine43@users.noreply.github.com>
2018-05-24 09:26:41 +1200

    Add files via upload

---
+++ director.py
@@ -0,0 +1,12 @@
+
+
+class Director:
+
+    def build_employee(self, builder):
+        builder.build_empid()
+        builder.build_gender()
+        builder.build_age()
+        builder.build_slaes()
+        builder.build_bmi()
+        builder.build_salary()
+        builder.build_birthday()
---
+++ employee_db_builder.py
@@ -0,0 +1,23 @@
+from employee import Employee
+
+
+class EmployeeDBBuilder:
+
+    my_employee = Employee()
+
+    def __init__(self, data):
+
+    def build_empid(self):
+
+    def build_gender(self):
+
+    def build_age(self):
+
+    def build_sales(self):
+
+    def build_bmi(self):
+
+    def build_salary(self):
+
+    def build_birthday(self):
+
